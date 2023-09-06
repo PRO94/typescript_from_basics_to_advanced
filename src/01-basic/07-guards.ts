5af28cc6288df011749d64c55bed08b82a32c31a
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-06 12:31:50 +0200

    Add guard functionality, compiled js file

---
+++ src/01-basic/07-guards.ts
@@ -0,0 +1,39 @@
+// ---------- Guards
+function strip(x: string | number) {
+    if (typeof x === 'number') {
+        return x.toFixed(2);
+    }
+    return x.trim();
+}
+
+// ==========
+class MyResponse {
+    header = 'response header';
+    result = 'response result';
+}
+
+class MyError {
+    header = 'error header';
+    message = 'error message';
+}
+
+function handle (res: MyResponse | MyError) {
+    if (res instanceof MyResponse) {
+        return {
+            info: res.header + res.result
+        };
+    }
+    return {
+        info: res.header + res.message
+    };
+}
+
+// ========== Safe types
+
+type AlertType = 'success' | 'danger' | 'warning';
+function setAlertType(type: AlertType) {
+    console.log(type);
+}
+// setAlertType('success');
+// setAlertType('warning');
+// setAlertType('someString');     // error
